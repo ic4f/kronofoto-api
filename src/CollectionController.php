ed53588fe84052e92e8d776970801389939fa9fd
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2018-03-16 13:55:32 -0400

    Add sorting for collections.
    Tests work for asc/desc ordering;
    Datetime fields may need more work.
    CollectionController ready for major refactoring.

--- src/CollectionController.php
+++ src/CollectionController.php
@@ -29,15 +29,16 @@ class CollectionController
         //$sort = $request->getQueryParam('sort');
         $qp = $request->getQueryParams();
 
+        $qb
+            ->select('*') //TODO change this
+            ->from('archive_collection');
+
         if (array_key_exists('sort', $qp)) {
             $sort = $qp['sort'];
 
             $order = 'ASC';
-            //is there ordering specified?
-            if ($sort[0] == '+' || $sort[0] == '-') {
-                if ($sort[0] == '-') {
-                    $order = 'DESC';
-                }
+            if ($sort[0] === '-') {
+                $order = 'DESC';
                 $sort = substr($sort, 1);
             }
 
@@ -45,28 +46,21 @@ class CollectionController
                 throw new \Exception("$sort is not a valid field");
             }
 
-            print("\n $sort and $order ");
 
-
-
-
-
-/*
             $qb
-                ->select('*') //TODO change this
-                ->from('archive_collection')
-                ->orderBy('
+                ->orderBy($sort, $order);
 
- */
+
 
         }
+        $stmt = $qb->execute();
 
 
 
 
-        $sql = 'select * from archive_collection limit 5';
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
+        //$sql = 'select * from archive_collection limit 5';
+        //$stmt = $conn->prepare($sql);
+        //$stmt->execute();
         $result = $stmt->fetchAll();
 
         return $response->withJson($result);
@@ -75,7 +69,7 @@ class CollectionController
     private function validateField($field)
     {
         $dbFields = 
-            ['id', 'name', 'year_min', 'year_max', 'item_count', 'is_published', 'created', 'modified', 'donor_id'];
+            ['id', 'name', 'year_min', 'year_max', 'item_count', 'is_published', 'created', 'modified', 'donor_id', 'featured_item_id'];
         return in_array($field, $dbFields);
     }
 
--- tests/api/CollectionsCest.php
+++ tests/api/CollectionsCest.php
@@ -6,29 +6,28 @@ class CollectionsCest
     const URL = '/collections';
     const COLLECTIONS_ALL = 296;
 
-    public function testJson(ApiTester $I)
+    public function testResponseIsJson(ApiTester $I)
     {
         $I->wantTo('get data in JSON format');
         $I->sendGET(self::URL); 
-        $this->checkJson($I);
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); //200
+        $I->seeHttpHeader('Content-type', 'application/json;charset=utf-8');
+        $I->seeResponseIsJson();
     }
 
     public function testRecordCount(ApiTester $I)
     {
         $count = self::COLLECTIONS_ALL;
-        $I->wantTo("get $count collections");
+        $I->wantTo("get $count records");
         $I->sendGET(self::URL); 
-        $this->checkJson($I);
-
         $data = $I->grabDataFromResponseByJsonPath('$*');
         $I->assertEquals($count, count($data));
     }
 
-    public function testDataFormat(ApiTester $I)
+    public function testDataStructure(ApiTester $I)
     {
-        $I->wantTo('get data in correct format');
+        $I->wantTo("check the structure of a record");
         $I->sendGET(self::URL); 
-        $this->checkJson($I);
 
         $I->seeResponseMatchesJsonType([
             'id' => 'integer',
@@ -45,52 +44,165 @@ class CollectionsCest
         ], '$*');
     }
 
-    public function testSortingYearMin(ApiTester $I) 
+    public function testSortedYearMinAcs(ApiTester $I) 
     {
-        $I->wantTo('get sorted by year_min');
-        $I->sendGET(self::URL . '?sort=year_min'); 
-        $this->checkJson($I);
-        $data = $I->grabDataFromResponseByJsonPath('$*');
+        $this->testSorted($I, 'year_min', false);
+    }
 
-        $this->checkIsSorted($I, $data, 'year_min');
+    public function testSortedYearMinDesc(ApiTester $I) 
+    {
+        $this->testSorted($I, 'year_min', true);
+    }
+
+    public function testSortedYearMaxAcs(ApiTester $I) 
+    {
+        $this->testSorted($I, 'year_max', false);
+    }
+
+    public function testSortedYearMaxDesc(ApiTester $I) 
+    {
+        $this->testSorted($I, 'year_max', true);
+    }
+
+    public function testSortedItemCountAcs(ApiTester $I) 
+    {
+        $this->testSorted($I, 'item_count', false);
+    }
+
+    public function testSortedItemCountDesc(ApiTester $I) 
+    {
+        $this->testSorted($I, 'item_count', true);
+    }
+
+    public function testSortedIsPublishedAcs(ApiTester $I) 
+    {
+        $this->testSorted($I, 'is_published', false);
+    }
+
+    public function testSortedIsPublishedDesc(ApiTester $I) 
+    {
+        $this->testSorted($I, 'is_published', true);
+    }
+
+    public function testSortedCreatedAcs(ApiTester $I) 
+    {
+        $this->testSorted($I, 'created', false);
+    }
+
+    public function testSortedCreatedDesc(ApiTester $I) 
+    {
+        $this->testSorted($I, 'created', true);
+    }
+
+    public function testSortedModifiedAcs(ApiTester $I) 
+    {
+        $this->testSorted($I, 'modified', false);
+    }
+
+    public function testSortedModifiedDesc(ApiTester $I) 
+    {
+        $this->testSorted($I, 'modified', true);
+    }
+ 
+    public function testSortedDonorIdAcs(ApiTester $I) 
+    {
+        $this->testSorted($I, 'donor_id', false);
+    }
+
+    public function testSortedDonorIdDesc(ApiTester $I) 
+    {
+        $this->testSorted($I, 'donor_id', true);
+    }
+ 
+    public function testSortedFeaturedItemIdAcs(ApiTester $I) 
+    {
+        $this->testSorted($I, 'featured_item_id', false);
+    }
+ 
+    public function testSortedFeaturedItemIdDesc(ApiTester $I) 
+    {
+        $this->testSorted($I, 'featured_item_id', true);
+    }
+ 
+
+    /* ------------------- private ----------------------- */
+
+    private function testSorted(ApiTester $I, $col, $isDesc)
+    {
+        $order = $isDesc ? 'descending' : 'ascending';
+        $I->wantTo("get records sorted by $col in $order order");
+
+        $desc = $isDesc ? '-' : '';
+        $I->sendGET(self::URL . "?sort=$desc$col"); 
+
+        $data = $I->grabDataFromResponseByJsonPath('$*');
+        $this->checkIsSorted($I, $data, $col, $isDesc);
+    }
+
+    private function checkIsSorted(ApiTester $I, $recordset, $col, $isDesc)
+    {
+        $previous = $isDesc ? 9999 : null;
+
+        //TODO: datetime fields may require special processing
+        foreach ($recordset as $row) {
+            $current= $row[$col];
+            if ($isDesc) {
+                $I->assertGreaterThanOrEqual($current, $previous);
+            } else {
+                $I->assertLessThanOrEqual($current, $previous);
+            }
+            $previous = $current;
+        }
     }
 
 
-    //public function testSortingYearMinAcs(ApiTester $I) 
-    //{
-    //    $I->wantTo('get sorted by year_min; ascending order');
-    //    $I->sendGET(self::URL . '?sort=+year_min'); 
-    //    $this->checkJson($I);
-    //    $data = $I->grabDataFromResponseByJsonPath('$*');
 
-    //    $this->checkIsSorted($I, $data, 'year_min');
-    //}
-
-
-    //public function testSortingYearMinDesc(ApiTester $I) 
-    //{
-    //    $I->wantTo('get sorted by year_min; descending order');
-    //    $I->sendGET(self::URL . '?sort=-year_min'); 
-    //    $this->checkJson($I);
-    //    $data = $I->grabDataFromResponseByJsonPath('$*');
-
-    //    $this->checkIsSorted($I, $data, 'year_min');
-    //}
-
-    private function checkIsSorted(ApiTester $I, $recordset, $col)
+/*
+    public function testSortingYearMaxAcs(ApiTester $I) 
     {
-        $current = $recordset[0][$col];
+        $I->wantTo('get records sorted by year_max in acsending order');
+        $I->sendGET(self::URL . '?sort=year_max'); 
+        $data = $I->grabDataFromResponseByJsonPath('$*');
+        $this->checkIsSortedAcs($I, $data, 'year_max');
+    }
+
+    public function testSortingYearMaxDesc(ApiTester $I) 
+    {
+        $I->wantTo('get records sorted by year_max in descending order');
+        $I->sendGET(self::URL . '?sort=-year_max'); 
+        $data = $I->grabDataFromResponseByJsonPath('$*');
+        $this->checkIsSortedDesc($I, $data, 'year_max');
+    }
+
+    public function testSortingItemCountAcs(ApiTester $I) 
+    {
+        $I->wantTo('get records sorted by item_count in acsending order');
+        $I->sendGET(self::URL . '?sort=item_count'); 
+        $data = $I->grabDataFromResponseByJsonPath('$*');
+        $this->checkIsSortedAcs($I, $data, 'item_count');
+    }
+
+    public function testSortingItemCountDesc(ApiTester $I) 
+    {
+        $I->wantTo('get records sorted by item_count in descending order');
+        $I->sendGET(self::URL . '?sort=-item_count'); 
+        $data = $I->grabDataFromResponseByJsonPath('$*');
+        $this->checkIsSortedDesc($I, $data, 'item_count');
+    }
+
+ */
+    private function bcheckIsSorted(ApiTester $I, $recordset, $col, $isDesc)
+    {
+        $previous = $isDesc ? 9999 : null;
+
         foreach ($recordset as $row) {
-            $next = $row[$col];
-            $I->assertLessThanOrEqual($current, $next);
-            $current = $next;
+            $current= $row[$col];
+            if ($isDesc) {
+                $I->assertGreaterThanOrEqual($current, $previous);
+            } else {
+                $I->assertLessThanOrEqual($current, $previous);
+            }
+            $previous = $current;
         }
     } 
-
-    private function checkJson(ApiTester $I) 
-    {
-        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); //200
-        $I->seeHttpHeader('Content-type', 'application/json;charset=utf-8');
-        $I->seeResponseIsJson();
-    }
 }
